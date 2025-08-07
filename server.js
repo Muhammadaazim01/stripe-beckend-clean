e6b645de49763e4027e30e7bea26923b7042bc25
Muhammadaazim01 <muhammadaazim96@gmail.com>
2025-08-07 06:10:20 +0500

    add start script for Railway deployment

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);const bodyParser = require('body-parser');
